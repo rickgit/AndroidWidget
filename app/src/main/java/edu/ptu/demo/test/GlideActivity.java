7749bd92ddb39d598f131646d6ec6525fe0b8b20
anshu.wang.work@gmail.com <anshu.wang.work@gmail.com>
2016-11-15 19:41:31 +0800

    project palette

--- app/src/main/java/edu/ptu/demo/test/GlideActivity.java
+++ app/src/main/java/edu/ptu/demo/test/GlideActivity.java
@@ -1,8 +1,11 @@
 package edu.ptu.demo.test;
 
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.graphics.Palette;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -20,7 +23,12 @@ public class GlideActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_glide);
         View iv = findViewById(R.id.iv);
-        Glide.with(this).load(R.drawable.ic_launcher)
+        View bg = findViewById(R.id.bg);
+        Glide.with(this).load(R.drawable.gs)
                 .into((ImageView) findViewById(R.id.iv));
+
+        BitmapDrawable drawable = (BitmapDrawable) getResources().getDrawable(R.drawable.gs);
+        Palette generate = Palette.from(drawable.getBitmap()).generate();
+        bg.setBackgroundColor(generate.getDarkMutedSwatch().getRgb());
     }
 }
