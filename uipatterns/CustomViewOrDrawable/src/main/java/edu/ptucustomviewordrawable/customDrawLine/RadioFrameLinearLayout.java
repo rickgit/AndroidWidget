71b3eeb122241df199eecf16159e654346a20300
anshu.wang.work@gmail.com <anshu.wang.work@gmail.com>
2017-07-07 18:54:33 +0800

    tooltip

--- uipatterns/CustomViewOrDrawable/src/main/java/edu/ptucustomviewordrawable/customDrawLine/RadioFrameLinearLayout.java
+++ uipatterns/CustomViewOrDrawable/src/main/java/edu/ptucustomviewordrawable/customDrawLine/RadioFrameLinearLayout.java
@@ -1,21 +1,22 @@
 package edu.ptucustomviewordrawable.customDrawLine;
 
 import android.annotation.TargetApi;
-import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
-import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.widget.LinearLayout;
 
+import java.lang.ref.SoftReference;
+
 import edu.ptucustomviewordrawable.DeviceUtils;
 import edu.ptucustomviewordrawable.LogUtils;
+import edu.ptucustomviewordrawable.R;
 
 
 /**
@@ -23,9 +24,13 @@ import edu.ptucustomviewordrawable.LogUtils;
  */
 
 public class RadioFrameLinearLayout extends LinearLayout {
-    Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     Paint mWPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private int childCount;
+    //FIXME 注意内存溢出
+    private Bitmap bitmap;
+    private Bitmap lastBitmap;
+    private Bitmap selectBitmap;
+    private Bitmap lastSelectBitmap;
 
     public RadioFrameLinearLayout(Context context) {
         super(context);
@@ -49,13 +54,11 @@ public class RadioFrameLinearLayout extends LinearLayout {
     }
 
     private void initView() {
-        mPaint.setColor(0xffcacaca);
-        mPaint.setStrokeWidth(5);
-        mPaint.setStyle(Paint.Style.STROKE);
         mWPaint.setColor(0xffffaacc);
-        mWPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OVER));
-        mWPaint.setStrokeWidth(5);
+//        mWPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OVER));
+        mWPaint.setStrokeWidth(2);
         mWPaint.setStyle(Paint.Style.STROKE);
+
 
     }
 
@@ -65,11 +68,6 @@ public class RadioFrameLinearLayout extends LinearLayout {
         drawSeparateLines(canvas);
     }
 
-    //    @Override
-//    protected void onDraw(Canvas canvas) {
-//        super.onDraw(canvas);
-//        drawSeparateLines(canvas);
-//    }
     private int select = -1;
 
     /**
@@ -86,55 +84,100 @@ public class RadioFrameLinearLayout extends LinearLayout {
 
         if (getOrientation() == HORIZONTAL) {
             LogUtils.logMainInfo("drawSeparateLines  getWidth" + DeviceUtils.getScreenSize(this)[0] + ": " + getWidth() + " :" + getPaddingRight());
-            float childWidth = (this.getWidth() - getPaddingLeft() - getPaddingRight()) / childCount;
-            RectF rectF = new RectF();
-            canvas.drawRect( getPaddingLeft(),getPaddingTop(),this.getWidth() - getPaddingLeft() - getPaddingRight(),getHeight()-getPaddingTop()-getPaddingBottom(),mPaint);
-            for (int i = 0; i < childCount-1; i++) {// { |█|█|█| } 共两条分割线
-                float startX = getPaddingLeft() + childWidth * i;
-                float endX = startX + childWidth;
+            for (int i = 0; i < childCount; i++) {// { |█|█|█| } 共两条分割线
+                boolean islast = i == childCount - 1;
 
-
-                LogUtils.logMainInfo("drawSeparateLines  startX" + startX + " " + getHeight());
-                if (select != i) {
-//                    if (select== -1 || select != i - 1) {
-//                        canvas.drawLine(startX, getHeight(), startX, 0, mWPaint);//左分割线
-//                        LogUtils.logMainInfo(String.format("drawSeparateLines 左分割线 %d  %d  %d   %d ", (int) startX, getHeight(), (int) startX, 0));
-//                    }
-//                    canvas.drawLine(startX, 0, endX, 0, mPaint);//上分割线
-//                    LogUtils.logMainInfo(String.format("drawSeparateLines 上分割线 %d  %d  %d   %d ", (int) startX, 0, (int) endX, 0));
-//                    canvas.drawLine(startX, getHeight(), endX, getHeight(), mPaint);//下分割线
-//                    LogUtils.logMainInfo(String.format("drawSeparateLines 下分割线 %d  %d  %d   %d ", (int) startX, getHeight(), (int) endX, getHeight()));
-//                    if (i == childCount - 1) {
-//                        canvas.drawLine(endX, getHeight(), endX, 0, mWPaint);//右分割分割线
-//                        LogUtils.logMainInfo(String.format("drawSeparateLines 右分割线  %d  %d  %d   %d ", (int) endX, getHeight(), (int) endX, 0));
-//                    }
-                    canvas.drawBitmap();
+                if (islast) {
+                    lastBitmap = getBitmap(islast);
+                    drawSelect(i, canvas, lastBitmap, mWPaint);
+                } else {
+                    bitmap = getBitmap(islast);
+                    drawSelect(i, canvas, bitmap, mWPaint);
                 }
             }
-            drawSelect(canvas,mWPaint);
+            if (select >= 0) {
+                boolean islast = select == childCount - 1;
+                LogUtils.logMainInfo("islast "+islast);
+                if (islast) {
+                    lastBitmap = getSelectBitmap(islast);
+                    drawSelect(select, canvas, lastBitmap, mWPaint);
+                } else {
+                    selectBitmap = getSelectBitmap(islast);
+                    drawSelect(select, canvas, selectBitmap, mWPaint);
+                }
+            }
 
         } else {//TODO　增加垂直方向的　segmentView
-            int childHeight = this.getHeight() / childCount;
-            for (int i = 1; i < childCount; i++) {
-                int startY = childHeight * i;
-                canvas.drawLine(0, startY, this.getWidth() - 0, startY, mPaint);
-            }
-            canvas.drawLine(0, 0, getWidth(), 0, mPaint);
-            canvas.drawLine(getWidth(), 0, getWidth(), getHeight(), mPaint);
-            canvas.drawLine(getWidth(), getHeight(), 0, getHeight(), mPaint);
-            canvas.drawLine(0, getHeight(), 0, 0, mPaint);
+
         }
 
     }
 
-    private void drawSelect(Canvas canvas, Paint mWPaint) {
-        canvas.save();
-//        canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+    private void drawSelect(int position, Canvas canvas, Bitmap drawable, Paint mWPaint) {
         float childWidth = (this.getWidth() - getPaddingLeft() - getPaddingRight()) / childCount;
-        float startX = getPaddingLeft() + childWidth * select;
+        float startX = getPaddingLeft() + childWidth * position;
         float endX = startX + childWidth;
-        canvas.drawRect(startX,getPaddingTop(),endX,getHeight()-getPaddingTop()-getPaddingBottom(),mWPaint);
-        canvas.restore();
+
+        canvas.drawBitmap(drawable, startX, 0, mWPaint);
+    }
+
+    public Bitmap drawableToBitmap(Drawable drawable, float paintStrokeWidth, boolean isLast) {
+        Bitmap bitmap = null;
+
+        if (drawable instanceof BitmapDrawable) {
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+            if (bitmapDrawable.getBitmap() != null) {
+                return bitmapDrawable.getBitmap();
+            }
+        }
+
+        if (drawable.getIntrinsicWidth() <= 0 || drawable.getIntrinsicHeight() <= 0) {
+            float childWidth = (this.getWidth() - getPaddingLeft() - getPaddingRight()) / childCount + (isLast ? (-mWPaint.getStrokeWidth()*2 ): mWPaint.getStrokeWidth());//加上drawable strokeWidth
+
+            int height = (int) (getHeight() - getPaddingTop() - getPaddingBottom());
+
+            bitmap = Bitmap.createBitmap((int) childWidth, height, Bitmap.Config.ARGB_8888);// Single color bitmap will be created of 1x1 pixel
+        } else {
+            float childWidth = (this.getWidth() - getPaddingLeft() - getPaddingRight() - paintStrokeWidth) / childCount;
+
+            int height = getHeight() - getPaddingTop() - getPaddingBottom();
+            LogUtils.logMainInfo("width " + childWidth + ": height " + height);
+            bitmap = Bitmap.createBitmap((int) childWidth, height, Bitmap.Config.ARGB_8888);
+        }
+
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bitmap;
+    }
+
+    /**
+     * 必须在onDraw初始化，才能获取到宽高
+     *
+     * @return
+     */
+    public Bitmap getBitmap(boolean islast) {
+        if (bitmap == null) {
+            bitmap = drawableToBitmap(getResources().getDrawable(R.drawable.rect_center), mWPaint.getStrokeWidth(), islast);
+//            bitmap = new SoftReference<Bitmap>(drawableToBitmap(getResources().getDrawable(R.drawable.rect_center), mWPaint.getStrokeWidth()));
+            LogUtils.logMainInfo("创建新对象");
+        }
+        return bitmap;
+    }
+
+    /**
+     * 必须在onDraw初始化，才能获取到宽高
+     *
+     * @return
+     */
+    public Bitmap getSelectBitmap(boolean islast) {
+        if (selectBitmap == null) {
+            selectBitmap = drawableToBitmap(getResources().getDrawable(R.drawable.rect_center_red), mWPaint.getStrokeWidth(), islast);
+//            selectBitmap = new SoftReference<Bitmap>(drawableToBitmap(getResources().getDrawable(R.drawable.rect_center_red), mWPaint.getStrokeWidth()));
+            LogUtils.logMainInfo("创建新对象");
+        }
+
+        return selectBitmap;
     }
 
     public void setSelect(int select) {
@@ -144,6 +187,5 @@ public class RadioFrameLinearLayout extends LinearLayout {
     public void unSelect() {
         this.select = -1;
     }
-
 
 }
