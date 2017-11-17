4e52bae9070d5901dab7c14a96cdc41ec212505f
anshu.wang <anshu.wang.work@gmail.com>
2017-11-17 17:54:49 +0800

    modify 优化滚动动画

--- uipatterns/CustomView/src/main/java/edu/ptu/customview/MainActivity.java
+++ uipatterns/CustomView/src/main/java/edu/ptu/customview/MainActivity.java
@@ -32,64 +32,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.tv_start).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                genView.startGen();
-//                child.setNumber(3,System.currentTimeMillis()+5000,300);
-                int dur = 1000;
-                Random random = new Random();
-                for (int i = 0; i < animators.size(); i++) {
-                    long currentPlayTime = animators.get(i).getCurrentPlayTime();
-                    if (animators.get(i).isRunning())
-                        return;
-                }
-                System.out.println("数字=");
-                System.out.println("数字");
-                for (int i = 0; i < whellView.size(); i++) {
-                    final int finalI = i;
-
-
-//                    int itemHeight = (int) whellView.get(finalI).getItemHeight();
-//                    int initValue = whellView.get(finalI).getCurrentItem() - 0;
-//                    whellView.get(finalI).setTotalScrollY(initValue*itemHeight+itemHeight*3);
-//                    whellView.get(finalI).invalidate();
-
-
-//                    int itemHeight = (int) whellView.get(i).getItemHeight();
-//                    int itemHeightStart = whellView.get(i).getCurrentItem() * itemHeight;
-//                    final int endTIme = itemHeightStart + itemHeight * 3;
-//                    ValueAnimator animator = ValueAnimator.ofInt(itemHeightStart, endTIme);
-//
-//                    animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-//                        @Override
-//                        public void onAnimationUpdate(final ValueAnimator valueAnimator) {
-//                            Integer animatedValue = (Integer) valueAnimator.getAnimatedValue();
-//
-//
-//                            whellView.get(finalI).setTotalScrollY(animatedValue);
-//                            whellView.get(finalI).invalidate();
-//                            if (endTIme==animatedValue)
-//                                whellView.get(finalI).smoothScroll(WheelView.ACTION.FLING);
-//                        }
-//                    });
-//                    animator.setDuration(5000);
-//                    animator.setInterpolator(new AccelerateDecelerateInterpolator());
-//                    animator.start();
-
-
-//                    animators.get(i).cancel();
-                    int currentItem = whellView.get(finalI).getCurrentItem();
-                    whellView.get(finalI).setCurrentItem(currentItem);
-
-                    animators.get(i).setDuration(dur);
-                    int num = random.nextInt(31) + 1;
-                    int nexValueDiff = num - (32 - currentItem);
-                    float change = -whellView.get(finalI).getItemHeight() * nexValueDiff - whellView.get(finalI).getItemHeight() * 32;
-                    System.out.println("数字 " + num);
-                    dur += 30 + random.nextInt(300);
-                    animators.get(finalI).setFloatValues(0, change);
-                    animators.get(i).start();
-                }
-                System.out.println("数字=");
-                System.out.println("数字");
+                startLotteryAnimation();
 
             }
         });
@@ -110,29 +53,63 @@ public class MainActivity extends AppCompatActivity {
         vg.addView(child);
 
         ViewGroup vgWheel = (ViewGroup) findViewById(R.id.wheelviews);
+        initLotteryView(vgWheel);
+
+
+    }
+    /**
+     *
+     * @param
+     * @return void
+     * @author anshu.wang 2017-11-17 17:52:42
+     **/
+    private void startLotteryAnimation() {
+        int dur = 1000;
+        Random random = new Random();
+        for (int i = 0; i < animators.size(); i++) {
+            if (animators.get(i).isRunning())
+                return;
+        }
+        System.out.println("数字");
+        System.out.println("数字");
+        for (int i = 0; i < whellView.size(); i++) {
+            final int finalI = i;
+            int currentItem = whellView.get(finalI).getCurrentItem();
+            whellView.get(finalI).setCurrentItem(currentItem);
+            int num = random.nextInt(31) + 1;
+            int nexValueDiff = num - (32 - currentItem);
+            int change = (int) (-whellView.get(finalI).getItemHeight() * nexValueDiff - whellView.get(finalI).getItemHeight() * 32);
+            System.out.println("数字 " + num);
+            dur += 30 + random.nextInt(300);
+            animators.get(i).setDuration(dur);
+            animators.get(finalI).setIntValues(0, change);
+            animators.get(i).start();
+        }
+        System.out.println("数字=");
+        System.out.println("数字");
+    }
+    /**
+     *
+     * @param vgWheel
+     * @return void
+     * @author anshu.wang 2017-11-17 17:52:56
+     **/
+    private void initLotteryView(ViewGroup vgWheel) {
         for (int i = 0; i < vgWheel.getChildCount(); i++) {
             final WheelView wheel = (WheelView) vgWheel.getChildAt(i);
             whellView.add(wheel);
-//            wheel.setAdapter(new NumericWheelAdapter(1, 32, "%02d"));
             wheel.setAdapter(new ArrayWheelAdapter(new ArrayList() {{
                 for (int j = 0; j < 32; j++) {
-
                     add(String.format("%02d", 32 - j));
                 }
             }}));
             wheel.setCurrentItem(31);
-            final int finalI = i;
-
-            float scrollLength = -wheel.getItemHeight() * vgWheel.getChildCount() - new Random().nextInt(32) * wheel.getItemHeight();
-            ValueAnimator animator = ValueAnimator.ofFloat(0, -1 * wheel.getItemHeight());
-            final int finalI1 = i;
+            ValueAnimator animator = ValueAnimator.ofInt(0,0);
             animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
                 @Override
-                public void onAnimationUpdate(final ValueAnimator valueAnimator) {
-//                    System.out.println("动画的值 "+ finalI1 +" : "+ valueAnimator.getAnimatedValue());
-//                    wheel.scrollBy((Float) valueAnimator.getAnimatedValue());
-                    Float animatedValue = (Float) valueAnimator.getAnimatedValue();
-                    wheel.setTotalScrollY((int) (float) animatedValue);
+                public void onAnimationUpdate( ValueAnimator valueAnimator) {
+                    int animatedValue = (int) valueAnimator.getAnimatedValue();
+                    wheel.setTotalScrollY(animatedValue);
                     wheel.invalidate();
                 }
             });
@@ -140,18 +117,8 @@ public class MainActivity extends AppCompatActivity {
             animator.setFloatValues();
             animator.setInterpolator(new AccelerateDecelerateInterpolator());
             animators.add(animator);
-
             if (i==vgWheel.getChildCount()-1||i==vgWheel.getChildCount()-2)
                 wheel.setTextPaintColor(0xff35a3ff);
         }
-//        whellView = (WheelView) vgWheelS;
-//        whellView.setAdapter(new ArrayWheelAdapter(new ArrayList(){
-//            {
-//                for (int i = 0; i < 32; i++) {
-//                    add(String.format("%02d",i));
-//                }
-//            }
-//        }));
-
     }
 }
